38dd43fe13e8299e21c3f6e57264a2ac6527ba84
Viktor Stoimenov <viktorstoimenov@vivaldi.net>
2024-07-01 13:28:50 +0300

    Code cleanup and mobile burger support

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,37 @@
 import React, { FC } from "react";
 import styles from "./NavBar.module.scss";
 import Logo from "../../assets/images/logo.svg?react";
+import IconMenu from "../../assets/images/icon-menu.svg?react";
+import IconMenuClose from "../../assets/images/icon-menu-close.svg?react";
 
 interface NavBarProps {}
 
-export const NavBar: FC<NavBarProps> = ({}) => {
+export const NavBar: FC<NavBarProps> = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const onMenuIconClick = () => {
+    setIsMenuOpen((isMenuOpen) => !isMenuOpen);
+  };
+
+  const iconMenuProps: React.SVGProps<SVGSVGElement> = {
+    className: styles.menuButton,
+    onClick: onMenuIconClick,
+  };
+
   return (
     <div className={styles.navBarContainer}>
       <Logo />
-      <ul>
+      {isMenuOpen ? (
+        <IconMenuClose {...iconMenuProps} />
+      ) : (
+        <IconMenu {...iconMenuProps} />
+      )}
+      {isMenuOpen && <div className={styles.overlay}></div>}
+      <ul
+        className={`${styles.navLinksContainer} ${
+          isMenuOpen ? "" : styles.closedMenu
+        }`}
+      >
         <li>
           <a href="#">Home</a>
         </li>
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import styles from "./MainPage.module.scss";
 import { NavBar } from "../../components/NavBar/NavBar";
 
 interface MainPageProps {}
 
-export const MainPage: FC<MainPageProps> = ({}) => {
+export const MainPage: FC<MainPageProps> = () => {
   return (
     <div className={styles.mainPageContainer}>
       <NavBar />
